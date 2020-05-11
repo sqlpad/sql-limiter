fdf8efd706c12e96130d014d482fddda415431d9
Rick Bergfalk <rick.bergfalk@gmail.com>
2020-05-11 11:56:38 -0500

    More test cleanup

--- test/enforce-limit.js
+++ test/enforce-limit.js
@@ -1,7 +1,7 @@
 const assert = require("assert");
 const utils = require("../src/utils");
 
-function enforceFirst(sqlText, expected) {
+function test(sqlText, expected) {
   const queriesTokens = utils.getQueriesTokens(sqlText);
   const queryTokens = utils.enforceLimit(queriesTokens[0], 1000);
   const enforcedSql = queryTokens.map((t) => t.text).join("");
@@ -10,67 +10,61 @@ function enforceFirst(sqlText, expected) {
 
 describe("enforceLimit", function () {
   it("basic limit not existing", function () {
-    enforceFirst(
-      `SELECT * FROM something`,
-      "SELECT * FROM something limit 1000"
-    );
+    test(`SELECT * FROM something`, "SELECT * FROM something limit 1000");
   });
 
   it("basic limit not existing with semi", function () {
-    enforceFirst(
-      `SELECT * FROM something;`,
-      "SELECT * FROM something limit 1000;"
-    );
+    test(`SELECT * FROM something;`, "SELECT * FROM something limit 1000;");
   });
 
   it("basic limit existing, under", function () {
-    enforceFirst(
+    test(
       `SELECT * FROM something LIMIT 10;`,
       "SELECT * FROM something LIMIT 10;"
     );
   });
 
   it("basic limit existing, over", function () {
-    enforceFirst(
+    test(
       `SELECT * FROM something LIMIT 9999;`,
       "SELECT * FROM something LIMIT 1000;"
     );
   });
 
   it("cte limit existing, over", function () {
-    enforceFirst(
+    test(
       `WITH cte AS (SELECT TOP 1 * FROM foo LIMIT 10) SELECT * FROM something LIMIT 9999 ;`,
       "WITH cte AS (SELECT TOP 1 * FROM foo LIMIT 10) SELECT * FROM something LIMIT 1000 ;"
     );
   });
 
   it("throws for unexpected limit", function () {
-    assert.throws(() => enforceFirst(`SELECT * FROM something limit`));
+    assert.throws(() => test(`SELECT * FROM something limit`));
   });
 
   it("handles offset", function () {
-    enforceFirst(
+    test(
       `SELECT * FROM something limit 9999 OFFSET 10`,
       `SELECT * FROM something limit 1000 OFFSET 10`
     );
   });
 
   it("handles trailing line comment", function () {
-    enforceFirst(
+    test(
       `SELECT * FROM something -- comment`,
       `SELECT * FROM something limit 1000 -- comment`
     );
   });
 
   it("handles offset no limit", function () {
-    enforceFirst(
+    test(
       `SELECT * FROM something OFFSET 10`,
       `SELECT * FROM something limit 1000 OFFSET 10`
     );
   });
 
   it("ignores non-select", function () {
-    enforceFirst(
+    test(
       `INSERT INTO foo SELECT * FROM something`,
       `INSERT INTO foo SELECT * FROM something`
     );
